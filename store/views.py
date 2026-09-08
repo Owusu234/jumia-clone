b153e311998f29a45ef9be6da4656489ec55905e
Owusu <owusufelix4004@gmail.com>
2026-09-08 22:37:21 +0000

    Initial commit

--- store/views.py
+++ store/views.py
@@ -1860,6 +1860,7 @@ def fix_glb_diffuse_factors(glb_bytes):
         return glb_bytes  # always safe
 
 @login_required
+@seller_required
 def upload_product(req):
     """Handle product upload with single category selection"""
     if req.method == "POST":
